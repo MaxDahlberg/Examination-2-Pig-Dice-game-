ad997c60ab45ed77c3e81da857a4079a17f99745
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2024-02-29 22:06:38 +0100

    Changed the design of the leaderboard again

--- pig (dice game)/Leaderboard.py
+++ pig (dice game)/Leaderboard.py
@@ -14,15 +14,15 @@ class Leaderboard:
 
         sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
 
-        print(f"{'╷'}{'=' * 30}{'╷'}")
-        print(f"{'|'}{'Leaderboard' :^30}{'|'}")
-        print(f"{'|'}{'=' * 30}{'|'}")
-        print(f"{'| '}{'Name':<15}{'Wins':<7}{'Win%':<7}{'|'}")
-        print(f"{'|'}{'=' * 30}{'|'}")
+        print(f"{'╭'}{'─' * 30}{'╮'}")
+        print(f"{'│'}{'Leaderboard' :^30}{'│'}")
+        print(f"{'│'}{'─' * 30}{'│'}")
+        print(f"{'│ '}{'Name':<15}{'Wins':<7}{'Win%':<7}{'│'}")
+        print(f"{'│'}{'─' * 30}{'│'}")
         for score in sorted_scores:
-            print(f"{'| '}{score[0]:<15}{score[1]:<7}{'100.0':<7}{'|'}")
+            print(f"{'│ '}{score[0]:<15}{score[1]:<7}{'100.0':<7}{'│'}")
 
-        print(f"{'╵'}{'=' * 30}{'╵'}")
+        print(f"{'╰'}{'─' * 30}{'╯'}")
 
 
 Leaderboard.print_leaderboard()

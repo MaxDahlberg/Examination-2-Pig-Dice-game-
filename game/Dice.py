3a62147629e5f2b33e13e4820cbd14474d2f286e
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2024-02-29 23:27:47 +0100

    Changed the dice rolls to print a dice symbol instead of just a number

--- game/Dice.py
+++ game/Dice.py
@@ -2,33 +2,32 @@ import random
 
 
 class Dice:
-    
     def roll_dice(round_total):
         roll = random.randint(1, 6)
 
         match roll:
             case 1:
-                print("1")
+                print("⚀")
                 round_total = 0
             
             case 2:
-                print("2")
+                print("⚁")
                 round_total += 2
 
             case 3:
-                print("3")
+                print("⚂")
                 round_total += 3
             
             case 4:
-                print("4")
+                print("⚃")
                 round_total += 4
             
             case 5:
-                print("5")
+                print("⚄")
                 round_total += 5
             
             case 6:
-                print("6")
+                print("⚅")
                 round_total += 6
             
             case _:

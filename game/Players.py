d2ec065a3e68fd77fdb0ff0ad71667c86e8a1fec
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2024-02-29 22:40:48 +0100

    Added Players class and methods for saving and loading players

---
+++ game/Players.py
@@ -0,0 +1,12 @@
+class Players():
+    players = [] #load all players into list
+
+    def load_players(self):
+        print("Loading players")
+    #load_players (When program starts)
+    #read from file and add to list of players
+
+    def save_players():
+        print("Saving players")
+    #save players (When program exits)
+    #read from list of players and override the file

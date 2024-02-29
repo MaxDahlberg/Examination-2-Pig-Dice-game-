1cd52bdc5f12c052c11b184e3b1cd7651be11690
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2024-02-29 22:41:23 +0100

    Made black

--- game/Players.py
+++ game/Players.py
@@ -1,12 +1,14 @@
-class Players():
-    players = [] #load all players into list
+class Players:
+    players = []  # load all players into list
 
     def load_players(self):
         print("Loading players")
-    #load_players (When program starts)
-    #read from file and add to list of players
+
+    # load_players (When program starts)
+    # read from file and add to list of players
 
     def save_players():
         print("Saving players")
-    #save players (When program exits)
-    #read from list of players and override the file
+
+    # save players (When program exits)
+    # read from list of players and override the file

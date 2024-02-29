f8578b496bede5488783494854698396b73449f4
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2024-02-29 22:21:24 +0100

    Added a player class

---
+++ pig (dice game)/Player.py
@@ -0,0 +1,27 @@
+class Player():
+    def __init__(self, name):
+        self.name = name
+        self.total_games = 0
+        self.total_wins = 0
+        self.win_percent = self.total_wins / self.total_games * 100 #total games can be 0. need to ensure we dont divide with 0
+
+    def change_name(self, name):
+        self.name = name
+
+    def add_total_game(self):
+        self.total_games += 1
+
+    def add_win(self):
+        self.total_wins += 1
+
+    def get_name(self):
+        return self.name
+    
+    def get_total_games(self):
+        return self.total_games
+    
+    def get_total_wins(self):
+        return self.total_wins
+    
+    def get_win_percent(self):
+        return self.win_percent

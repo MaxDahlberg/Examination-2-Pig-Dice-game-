17e609d6bf344ef0c0bafa950e505544363229dd
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2024-02-29 23:35:10 +0100

    Changed the dice design to a custom made one to ensure that its easally readable

--- game/Dice.py
+++ game/Dice.py
@@ -7,27 +7,27 @@ class Dice:
 
         match roll:
             case 1:
-                print("⚀")
+                print(Dice.face_1())
                 round_total = 0
             
             case 2:
-                print("⚁")
+                print(Dice.face_2())
                 round_total += 2
 
             case 3:
-                print("⚂")
+                print(Dice.face_3())
                 round_total += 3
             
             case 4:
-                print("⚃")
+                print(Dice.face_4())
                 round_total += 4
             
             case 5:
-                print("⚄")
+                print(Dice.face_5())
                 round_total += 5
             
             case 6:
-                print("⚅")
+                print(Dice.face_6())
                 round_total += 6
             
             case _:
@@ -36,4 +36,60 @@ class Dice:
         return round_total
 
 
-Dice.roll_dice(round_total=0)
+    def face_1():
+        face_1 = """╭─────────╮
+│         │
+│    ●    │
+│         │
+╰─────────╯
+"""
+        return face_1
+
+    def face_2():
+        face_2 = """╭─────────╮
+│  ●      │
+│         │
+│      ●  │
+╰─────────╯
+"""
+        return face_2
+
+    def face_3():
+        face_3 = """╭─────────╮
+│  ●      │
+│    ●    │
+│      ●  │
+╰─────────╯
+"""
+        return face_3
+
+    def face_4():
+        face_4 = """╭─────────╮
+│  ●   ●  │
+│         │
+│  ●   ●  │
+╰─────────╯
+"""
+        return face_4
+
+    def face_5():
+        face_5 = """╭─────────╮
+│  ●   ●  │
+│    ●    │
+│  ●   ●  │
+╰─────────╯
+"""
+        return face_5
+
+    def face_6():
+        face_6 = """╭─────────╮
+│  ●   ●  │
+│  ●   ●  │
+│  ●   ●  │
+╰─────────╯
+"""
+        return face_6
+    
+
+
+Dice.roll_dice(round_total=0) # just here for testing of the class

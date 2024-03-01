f3754edb957cea42d60596a8ac5cdf005d6bd1dd
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2024-03-01 10:56:30 +0100

    Made black

--- game/Game.py
+++ game/Game.py
@@ -1,13 +1,13 @@
 from Leaderboard import Leaderboard
 from Dice import Dice
 
+
 class Game:
     print()
     print()
-    Leaderboard.print_leaderboard() # just here for now to test the look of the game 
+    Leaderboard.print_leaderboard()  # just here for now to test the look of the game
     print()
     print()
-    
 
     def game_menu():
         game_total = 0
@@ -30,12 +30,12 @@ class Game:
             match choice:
                 case "1":
                     round_total = Dice.roll_dice(round_total, cheat)
-                
+
                 case "2":
                     game_total += round_total
                     round_total = 0
                     # end turn
-                
+
                 case "3":
                     if cheat:
                         print("Cheat off\n")
@@ -46,12 +46,13 @@ class Game:
 
                 case "4":
                     keep_running = False
-                
+
                 case _:
                     print("Not a valid choice")
 
             if game_total >= 100:
                 keep_running = False
                 print("You won!")
+
 
 Game.game_menu()

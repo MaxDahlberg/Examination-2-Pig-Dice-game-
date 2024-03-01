694e67c3a556b24d12b463079fda16148f8ef1cb
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2024-03-01 12:28:29 +0100

    Made the cheat toggle and wrong input messages look nicer

--- game/Game.py
+++ game/Game.py
@@ -21,10 +21,10 @@ class Game:
             print(f"{'│'}{'Round total:' :>18}{round_total:^13}{'│'}")
             print(f"{'╰'}{'─' * 31}{'╯'}\n")
             print(f"{'╭'}{'─' * 31}{'╮'}")
-            print(f"{'│'}{'        1. Roll Dice' :<31}{'│'}")
-            print(f"{'│'}{'        2. Hold' :<31}{'│'}")
-            print(f"{'│'}{'        3. Toggle Cheat' :<31}{'│'}")
-            print(f"{'│'}{'        4. Exit Game' :<31}{'│'}")
+            print(f"{'│' :<9}{'1. Roll Dice' :<23}{'│'}")
+            print(f"{'│' :<9}{'2. Hold' :<23}{'│'}")
+            print(f"{'│' :<9}{'3. Toggle Cheat' :<23}{'│'}")
+            print(f"{'│' :<9}{'4. Exit Game' :<23}{'│'}")
             print(f"{'╰'}{'─' * 31}{'╯'}\n")
             choice = input(">> ")
             print()
@@ -40,17 +40,24 @@ class Game:
 
                 case "3":
                     if cheat:
-                        print("Cheat off\n")
+                        print(f"{'╭' :>9}{'─' * 15}{'╮'}")
+                        print(f"{'│' :>9}{'Cheat off' :^15}{'│'}")
+                        print(f"{'╰' :>9}{'─' * 15}{'╯'}\n")
                         cheat = False
                     else:
-                        print("Cheat on\n")
+                        print(f"{'╭' :>9}{'─' * 15}{'╮'}")
+                        print(f"{'│' :>9}{'Cheat on' :^15}{'│'}")
+                        print(f"{'╰' :>9}{'─' * 15}{'╯'}\n")
                         cheat = True
 
                 case "4":
                     keep_running = False
 
                 case _:
-                    print("Not a valid choice")
+                    #print("Not a valid choice")
+                    print(f"{'╭' :>5}{'─' * 23}{'╮'}")
+                    print(f"{'│' :>5}{'Not a valid choice' :^23}{'│'}")
+                    print(f"{'╰' :>5}{'─' * 23}{'╯'}\n")
 
             if game_total >= 100:
                 keep_running = False

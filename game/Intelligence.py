769d0493830e78997048f59dba6bcaee08a574a2
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2024-03-02 14:22:41 +0100

    Created a function with repeat code for the difficulties that then gets used to loop through with conditions for each difficulty level

--- game/Intelligence.py
+++ game/Intelligence.py
@@ -2,54 +2,41 @@ from Dice import Dice
 
 
 class Intelligence:
-    difficulty_easy = False
+    difficulty_easy = True  # hard coded to true for now just for testing of the function before inmplementation of difficulty selection
     difficulty_medium = False
-    difficulty_hard = True
+    difficulty_hard = False
     computer_round_total = 0
     computer_game_total = 0
 
     def check_difficulty():
         if Intelligence.difficulty_easy:
-            Intelligence.easy_player()
+            for i in range(2):
+                Intelligence.play_computer_moves()
+                if Intelligence.computer_round_total == 0:
+                    print("Computer rolled a 1")
+                    break
+
         elif Intelligence.difficulty_medium:
-            Intelligence.medium_player()
+            while Intelligence.computer_round_total < 12:
+                Intelligence.play_computer_moves()
+                if Intelligence.computer_round_total == 0:
+                    print("Computer rolled a 1")
+                    break
+
         elif Intelligence.difficulty_hard:
-            Intelligence.hard_player()
+            while Intelligence.computer_round_total < 25:
+                Intelligence.play_computer_moves()
+                if Intelligence.computer_round_total == 0:
+                    print("Computer rolled a 1")
+                    break
         else:
             print("No difficulty chosen")
 
-    def easy_player():
-        for i in range(2):
-            Intelligence.computer_round_total = Dice.roll_dice(
-                Intelligence.computer_round_total, False
-            )
-            if Intelligence.computer_round_total == 0:
-                print("Computer rolled a 1")
-                break
-
         Intelligence.computer_game_total += Intelligence.computer_round_total
         Intelligence.computer_round_total = 0
+        print(Intelligence.computer_game_total)  # temp for testing
 
-    def medium_player():
-        while Intelligence.computer_round_total < 12:
-            Intelligence.computer_round_total = Dice.roll_dice(
-                Intelligence.computer_round_total, False
-            )
-            if Intelligence.computer_round_total == 0:
-                print("Computer rolled a 1")
-                break
-
-        Intelligence.computer_game_total += Intelligence.computer_round_total
-        Intelligence.computer_round_total = 0
-
-    def hard_player():
-        while Intelligence.computer_round_total < 25:
-            Intelligence.computer_round_total = Dice.roll_dice(
-                Intelligence.computer_round_total, False
-            )
-            if Intelligence.computer_round_total == 0:
-                print("Computer rolled a 1")
-                break
-
-        Intelligence.computer_game_total += Intelligence.computer_round_total
-        Intelligence.computer_round_total = 0
+    def play_computer_moves():
+        Intelligence.computer_round_total = Dice.roll_dice(
+            Intelligence.computer_round_total, False
+        )
